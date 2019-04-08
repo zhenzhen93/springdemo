6f9b730fcfe966183ddacafce4897cbc633ea419
zhenzhen.xie <zhenzhen.xie@zhangmen.com>
2019-04-08 14:10:38 +0800

    第一次提交

---
+++ src/main/java/com/miaoshaproject/App.java
@@ -0,0 +1,26 @@
+package com.miaoshaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ */
+//@EnableAutoConfiguration
+@RestController
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class App {
+
+    @RequestMapping("/")
+    public String home() {
+        return "helloWorld!!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SpringApplication.run(App.class, args);
+    }
+}
