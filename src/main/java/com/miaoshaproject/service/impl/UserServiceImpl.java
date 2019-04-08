bc139308da7976dc81a0f0ee0a27e352b129aa3b
zhenzhen.xie <zhenzhen.xie@zhangmen.com>
2019-04-08 21:17:22 +0800

    MVC框架

---
+++ src/main/java/com/miaoshaproject/service/impl/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.miaoshaproject.service.impl;
+
+import com.miaoshaproject.dao.UserDOMapper;
+import com.miaoshaproject.dao.UserPasswordDOMapper;
+import com.miaoshaproject.dataobject.UserDO;
+import com.miaoshaproject.dataobject.UserPasswordDO;
+import com.miaoshaproject.service.UserService;
+import com.miaoshaproject.service.model.UserModel;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDOMapper userDOMapper;
+
+    @Autowired
+    private UserPasswordDOMapper userPasswordDOMapper;
+
+
+    @Override
+    public UserModel getUserById(Integer id) {
+        UserDO userDO = userDOMapper.selectByPrimaryKey(id);
+        if (userDO == null) {
+            return null;
+        }
+        UserPasswordDO userPassword = userPasswordDOMapper.selectByUserId(id);
+        return convertFromDataObject(userDO, userPassword);
+    }
+
+    private UserModel convertFromDataObject(UserDO userDO, UserPasswordDO userPassword) {
+        if (userDO == null) {
+            return null;
+        }
+        UserModel userModel = new UserModel();
+        BeanUtils.copyProperties(userDO, userModel);
+        if (userPassword != null) {
+            userModel.setEncryptPassword(userPassword.getEncryptPassword());
+        }
+        return userModel;
+    }
+}
