2d026d45d79c6b3b4450402f341f05eb5f9c398e
zhenzhen.xie <zhenzhen.xie@zhangmen.com>
2019-04-08 20:35:34 +0800

    测试数据库查询功能

--- src/main/java/com/miaoshaproject/App.java
+++ src/main/java/com/miaoshaproject/App.java
@@ -1,5 +1,9 @@
 package com.miaoshaproject;
 
+import com.miaoshaproject.dao.UserDOMapper;
+import com.miaoshaproject.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -11,12 +15,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 //@EnableAutoConfiguration
 @RestController
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication(scanBasePackages = {"com.miaoshaproject"})
+@MapperScan("com.miaoshaproject.dao")
 public class App {
+
+    @Autowired
+    private UserDOMapper userDOMapper;
 
     @RequestMapping("/")
     public String home() {
-        return "helloWorld!!";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if (userDO == null) {
+            return "用户对象不存在";
+        } else {
+            return userDO.getName();
+        }
     }
 
     public static void main(String[] args) {
